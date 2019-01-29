f1dccaeb8f2d7ef0bd1b079f6a7787533e15d111
msunkist99 <msunkist99@gmail.com>
2019-01-28 18:56:03 -0600

    20190129 initial code

--- RandomMax/Program.cs
+++ RandomMax/Program.cs
@@ -7,6 +7,19 @@ namespace RandomMax
         public static void Main()
         {
             // TODO: Generate a list of 100 random integers between 0 and 1000 and ouput the maximum value.
+            Random random = new Random();
+            int largestRandomNumber = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                int randomNumber = random.Next(0, 1000);
+                if (randomNumber > largestRandomNumber)
+                {
+                    largestRandomNumber = randomNumber;
+                }
+            }
+
+            Console.WriteLine("Largest random number - " + largestRandomNumber);
 
             Console.ReadLine();
         }
